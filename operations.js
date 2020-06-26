ecabdc00a15cf0066e32d7f1de3f295c7e7d0507
Alan-Marx <alanmarx@hotmail.com>
2020-06-26 13:48:00 -0700

    implemented the validateNumbers function

--- operations.js
+++ operations.js
@@ -2,11 +2,12 @@ const add = (a, b) => +a + +b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
-
+const validateNumbers = (a, b) => !(isNaN(a) || isNaN(b));
 
 module.exports = { 
   add,
   subtract,
   multiply,
-  divide 
+  divide,
+  validateNumbers
 };

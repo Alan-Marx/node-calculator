d50d64de3567ffcde43beccc46ee4f369ca2eea8
Alan-Marx <alanmarx@hotmail.com>
2020-06-26 17:34:00 -0700

    adding a recursive looping component to the function and changed the way text is formatted to not use template literals

--- calc.js
+++ calc.js
@@ -7,43 +7,33 @@ const possibleOperations = {
   '3': { result: 'product',    operation: multiply },
   '4': { result: 'quotient',   operation: divide }
 }
+
+const caclApp = () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
   
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-console.log(`
-Calc.js
-
-Welcome to the Node.js Calculator app!
-Version: 1.0.0.
-
-Usage: The user will be prompted for two numbers, 
-then asked to select their operation of choice.
-`);
-
-rl.question('Enter the first number: ', a => {
-  rl.question('Enter the second number: ', b => {
-    rl.question(
-      `
-Please select from the following options:
-
-[1] Addition (+)
-[2] Subtraction (-)
-[3] Multiplication (x)
-[4] Division (/)
-
-Enter your choice: `, choice => {
-      if (!validateNumbers(a, b)) {
-        console.log('Only numbers are allowed! Please restart the program.')
-      } else if (![1, 2, 3, 4].includes(Number(choice))) {
-        console.log('You must select an option between 1 and 4! Please restart the program')
-      } else {
-        console.log(`The ${possibleOperations[choice].result} of ${a} and ${b} is ${possibleOperations[choice].operation(a, b)}`);
+  console.log('\nCalc.js\n\nWelcome to the Node.js Calculator app!\nVersion: 1.0.0.\n\nUsage: The user will be prompted for two numbers,\nthen asked to select their operation of choice.\n');
+  
+  rl.question('Enter the first number: ', a => {
+    rl.question('\nEnter the second number: ', b => {
+      rl.question('\nPlease select from the following options:\n\n[1] Addition (+)\n[2] Subtraction (-)\n[3] Multiplication (x)\n[4] Division (/)\n\nEnter your choice: ', choice => {
+        if (!validateNumbers(a, b)) {
+          console.log('\nOnly numbers are allowed!')
+        } else if (![1, 2, 3, 4].includes(Number(choice))) {
+          console.log('\nYou must select an option between 1 and 4!')
+        } else {
+          console.log(`\nThe ${possibleOperations[choice].result} of ${a} and ${b} is ${possibleOperations[choice].operation(a, b)}`);
+        }
+        rl.question('\nWould you like to perform another calculation? (y/n): ', answer => {
+          rl.close();
+          if (answer === 'y') caclApp();
+        })
       }
-      rl.close();
-    }
-    )
+      )
+    })
   })
-})
+}
+
+caclApp();

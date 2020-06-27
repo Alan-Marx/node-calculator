7bed9b3806d34a1ee98d62e72835c58426103113
Alan-Marx <alanmarx@hotmail.com>
2020-06-26 17:15:30 -0700

    added logic to calc.js to ensure the app prints out the correct result based on the operation chosen

--- calc.js
+++ calc.js
@@ -1,7 +1,13 @@
-const operations = require('./operations');
-
+const { add, subtract, multiply, divide, validateNumbers } = require('./operations');
 const readline = require('readline');
 
+const possibleOperations = {
+  '1': { result: 'sum',        operation: add },
+  '2': { result: 'difference', operation: subtract },
+  '3': { result: 'product',    operation: multiply },
+  '4': { result: 'quotient',   operation: divide }
+}
+  
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -21,18 +27,23 @@ rl.question('Enter the first number: ', a => {
   rl.question('Enter the second number: ', b => {
     rl.question(
       `
-    Please select from the following options:
+Please select from the following options:
 
-    [1] Addition (+)
-    [2] Subtraction (-)
-    [3] Multiplication (x)
-    [4] Division (/)
+[1] Addition (+)
+[2] Subtraction (-)
+[3] Multiplication (x)
+[4] Division (/)
 
-    Enter your choice: `, 
-      choice => {
-        console.log('\tHello!');
-        rl.close();
+Enter your choice: `, choice => {
+      if (!validateNumbers(a, b)) {
+        console.log('Only numbers are allowed! Please restart the program.')
+      } else if (![1, 2, 3, 4].includes(Number(choice))) {
+        console.log('You must select an option between 1 and 4! Please restart the program')
+      } else {
+        console.log(`The ${possibleOperations[choice].result} of ${a} and ${b} is ${possibleOperations[choice].operation(a, b)}`);
       }
+      rl.close();
+    }
     )
   })
 })
